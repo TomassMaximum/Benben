98fa65c93f2d651b7267ff691e2de6dc635d2247
魏保平 <2218607244@qq.com>
2022-07-31 16:45:22 +0800

    文件名遵从小写规则修改

--- lib/main/event_item.dart
+++ lib/main/event_item.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 enum Menu { mark, share, delete }
@@ -25,7 +27,7 @@ class EventItem extends StatelessWidget {
                 children: [
                   Container(
                     alignment: Alignment.center,
-                      child: Text(DateTime.now().toString())
+                      child: Text("${DateTime.now().month.toString()}月${DateTime.now().day.toString()}日 ${getWeekday(DateTime.now().weekday)}")
                   ),
                   Positioned(
                     right: 0,
@@ -70,6 +72,28 @@ class EventItem extends StatelessWidget {
       ),
 
     );
+  }
+
+  String getWeekday(int weekday) {
+    switch(weekday) {
+      case 1:
+        return "星期一";
+      case 2:
+        return "星期二";
+      case 3:
+        return "星期三";
+      case 4:
+        return "星期四";
+      case 5:
+        return "星期五";
+      case 6:
+        return "星期六";
+      case 7:
+        return "星期日";
+        default:
+          log("Error! weekday unexpected: ${weekday.toString()}");
+          return "N/A";
+    }
   }
 
 }
--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,9 +1,9 @@
-import 'package:benben/about_app/AboutAppPage.dart';
-import 'package:benben/about_author/AboutAuthorPage.dart';
-import 'package:benben/main/EventItem.dart';
-import 'package:benben/edit/InsertPage.dart';
-import 'package:benben/settings_notification/NotificationPage.dart';
-import 'package:benben/main/SettingsItem.dart';
+import 'package:benben/about_app/about_app_page.dart';
+import 'package:benben/about_author/about_author_page.dart';
+import 'package:benben/main/event_item.dart';
+import 'package:benben/edit/insert_page.dart';
+import 'package:benben/settings_notification/notification_page.dart';
+import 'package:benben/main/settings_item.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,21 +13,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Benben',
+      title: '小本本',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.brown,
       ),
       home: const MyHomePage(title: '小本本'),
@@ -37,15 +27,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -61,16 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       drawer: Container(
