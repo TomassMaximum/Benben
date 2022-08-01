b200d22bcc1cea77528052ef56f6909d7ecccd1f
魏保平 <2218607244@qq.com>
2022-08-01 13:33:40 +0800

    ios添加相册权限描述，添加图片功能调通

--- lib/edit/insert_page.dart
+++ lib/edit/insert_page.dart
@@ -4,8 +4,18 @@ import 'package:benben/edit/number_input.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class InsertPage extends StatelessWidget {
+class InsertPage extends StatefulWidget {
   const InsertPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return InsertPageState();
+  }
+}
+
+class InsertPageState extends State<InsertPage> {
+
+  String _imagePath = "";
 
   @override
   Widget build(BuildContext context) {
@@ -26,20 +36,26 @@ class InsertPage extends StatelessWidget {
         color: const Color(0xfffedbd0),
         alignment: Alignment.topCenter,
         padding: const EdgeInsets.only(left: 24, top: 24, right: 24),
-        child: Column(
+        child: ListView(
           children: [
             const NumberInput(),
             TextButton(
                 onPressed: () async {
                   final ImagePicker _picker = ImagePicker();
                   final XFile? file = await _picker.pickImage(source: ImageSource.gallery);
+                  if(file != null) {
+                    setState(() {
+                      _imagePath = file.path;
+                    });
+                  }
                 },
                 child: const Icon(Icons.add_a_photo)
-            )
+            ),
+            if(_imagePath.isNotEmpty) Image.file(File(_imagePath))
           ],
         ),
       ),
     );
   }
-}
 
+}
